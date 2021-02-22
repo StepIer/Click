bb8eab94a54c718326e520b49b0e582e477a2e63
Stepler <stephankondratev@gmail.com>
2021-02-22 22:06:53 +0300

    new commit

--- app/src/main/java/com/myproject/click/MainActivity.kt
+++ app/src/main/java/com/myproject/click/MainActivity.kt
@@ -1,13 +1,11 @@
 package com.myproject.click
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        Toast.makeText(this, "Sjadvk", Toast.LENGTH_SHORT).show()
     }
 }
