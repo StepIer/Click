c382dd5321cf0c07106068151e7b1c066d633fc7
Stepler <stephankondratev@gmail.com>
2021-02-22 23:31:36 +0300

    nothing

--- app/src/main/java/com/myproject/click/MainActivity.kt
+++ app/src/main/java/com/myproject/click/MainActivity.kt
@@ -1,11 +1,30 @@
 package com.myproject.click
 
+import android.graphics.Color
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.constraintlayout.widget.ConstraintLayout
 
 class MainActivity : AppCompatActivity() {
+    /*val - final
+    var - common
+    i?:0
+    */
+    lateinit var btnStart: Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        btnStart = findViewById(R.id.btn_start)
+        btnStart.setOnClickListener{
+            Toast.makeText(this, "Start!", Toast.LENGTH_SHORT).show()
+        }
+        val t = TextView(this).apply { text = "сонаос" }
+
+        findViewById<ConstraintLayout>(R.id.texttipo).addView(t)
+
     }
 }
